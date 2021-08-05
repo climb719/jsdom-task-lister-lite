745062ab97a1a0b45b80b9caae7580ef036c9b7c
climb719 <mary.oldweiler@gmail.com>
2021-08-05 14:53:48 -0400

    should have been committing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+   
+  function handleSubmit(e) {
+    e.preventDefault()
+    const userToDo = document.querySelector('form')["new-task-description"].value
+    //document.getElementById('list').innerHTML += (`<li>${userToDo}</li>`)
+    addToDo(`${userToDo}`)
+
+    console.log(`${userToDo}`)
+  } 
+
+
+  function addToDo(userToDo) {
+    let list = document.getElementById('list')
+   // list.innerHTML += document.querySelector('form')["new-task-description"].value
+    list.innerHTML += `<li>${userToDo}</li>` 
+  }
+  
+document.querySelector('form').addEventListener('submit', handleSubmit)
+
 });
