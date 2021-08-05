1eeaacc42a0e3a110044d7b52ee4f8aa1c14b27c
climb719 <mary.oldweiler@gmail.com>
2021-08-05 15:51:45 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -9,13 +9,26 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(`${userToDo}`)
   } 
 
-
   function addToDo(userToDo) {
     let list = document.getElementById('list')
    // list.innerHTML += document.querySelector('form')["new-task-description"].value
-    list.innerHTML += `<li>${userToDo} <button type="button">x</button> </li>` 
+    list.innerHTML += `<li>${userToDo} <button id="button">x</button> </li>` 
   }
+
   
+
+  // function handleButton(e) {
+  //  document.querySelector('li').remove()
+  // }
+
+
 document.querySelector('form').addEventListener('submit', handleSubmit)
+
+document.getElementById("button").addEventListener('click', function() {
+  document.querySelector('li').remove()
+ })
+ 
+// document.getElementById("button").addEventListener('click', handleButton)
+
 
 });
